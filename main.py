a8a11192c74a9547e8be59b24012c6fbf88ae569
Drew Emery <abemery@clemson.edu>
2019-06-19 21:36:04 -0400

    added highest historical worth to json file

--- main.py
+++ main.py
@@ -12,60 +12,62 @@ class Drew():
             account = json.load(f)
         self.sid = account['accountSID']
         self.token = account['authToken']
-        self.client = Client(self.sid, self.token)
         self.number = account['phoneNumber']
 
-def fetch():
-    # get the
-    r = requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
-    r = r.json()[0]
-    with open('h.json', 'w') as f:
-        json.dump(r, f)
-    return r
+    def fetch(self):
+        # get the
+        r = requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
+        r = r.json()[0]
+        with open('h.json', 'w') as f:
+            json.dump(r, f)
+        return r
 
 
-def compare(r):
+    def compare(self, r):
 
-    # load our account detail
-    with open('account.json') as f:
-        account = json.load(f)
+        # load our account detail
+        with open('account.json') as f:
+            account = json.load(f)
 
-    # the current price of bitcoin
-    price = float(r['price_usd'])
+        # the current price of bitcoin
+        price = float(r['price_usd'])
 
-    # the amount bought and the price it was purchased for
-    quantityBought = account['quantityBought']
-    purchasePrice = account['purchasePrice']
-    highestWorth = account['highestWorth']
+        # the amount bought and the price it was purchased for
+        quantityBought = account['quantityBought']
+        purchasePrice = account['purchasePrice']
+        highestWorth = account['highestWorth']
 
-    selloutPrice = account['highestWorth'] * .8
+        selloutPrice = account['highestWorth'] * .95
 
-    # calculate the current worth in dollars of bitcoin quantity
-    worth = price * quantityBought
+        # calculate the current worth in dollars of bitcoin quantity
+        worth = price * quantityBought
 
-    if worth > highestWorth:
-        account['highestWorth'] = worth
-        with open('account.json', 'w') as f:
-            account = json.dump(account, f)
+        if worth > highestWorth:
+            account['highestWorth'] = worth
+            with open('account.json', 'w') as f:
+                account = json.dump(account, f)
 
-    if worth < selloutPrice:
-        sendPriceAlert(self.number, '~-~ CHECK BITCOIN WALLET ~-~')
-        exit()
-    difference = worth - purchasePrice
+        if worth < selloutPrice:
+            self.sendPriceAlert(self.number, '~\\\\~ CHECK BITCOIN WALLET ~//~')
+            exit()
+        difference = worth - purchasePrice
 
-    # print the gain / loss of the purhcase
-    if difference > 0:
-        print('{}\tMONEY GAINED:\t$ {}'.format(datetime.datetime.now(), difference))
-    if difference < 0:
-        print('MONEY LOST:\t$ {}'.format(datetime.datetime.now(), difference))
+        # print the gain / loss of the purhcase
+        if difference > 0:
+            print('{}\tMONEY GAINED:\t$ {}'.format(datetime.datetime.now(), difference))
+        if difference < 0:
+            print('MONEY LOST:\t$ {}'.format(datetime.datetime.now(), difference))
 
-def sendPriceAlert(number, message):
-    message = client.messages.create(
-        to=number,
-        from_="+19389999671",
-        body=message)
+    def sendPriceAlert(self, number, message):
+        client = Client(self.sid, self.token)
+        message = client.messages.create(
+            to=number,
+            from_="+19389999671",
+            body=message)
 
 if __name__ == '__main__':
+
+    drew = Drew()
     while True:
-        compare(fetch())
-        time.sleep(30)
+        drew.compare(drew.fetch())
+        time.sleep(15)
