eb05d2cdb89a94bd6050549bfbe3243dfc3b7ad4
Drew Emery <abemery@clemson.edu>
2019-06-21 15:06:03 -0400

    added sampleAccount.json; just needs to be populated with user's specific values

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ from twilio.rest import Client
 class Drew():
     def __init__(self):
 
-        with open('account.json') as f:
-            account = json.load(f)
-        self.sid = account['accountSID']
-        self.token = account['authToken']
-        self.number = account['phoneNumber']
+        with open('account.json', 'r') as f:
+            self.account = json.load(f)
+
+        self.sid = self.account['accountSID']
+        self.token = self.account['authToken']
+        self.number = self.account['phoneNumber']
 
     def fetch(self):
         # get the
         r = requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
         r = r.json()[0]
+
         with open('h.json', 'w') as f:
             json.dump(r, f)
         return r
@@ -25,28 +27,30 @@ class Drew():
 
     def compare(self, r):
 
-        # load our account detail
-        with open('account.json') as f:
-            account = json.load(f)
-
         # the current price of bitcoin
         price = float(r['price_usd'])
 
         # the amount bought and the price it was purchased for
-        quantityBought = account['quantityBought']
-        purchasePrice = account['purchasePrice']
-        highestWorth = account['highestWorth']
+        quantityBought = self.account['quantityBought']
+        purchasePrice = self.account['purchasePrice']
+        highestWorth = self.account['highestWorth']
 
-        selloutPrice = account['highestWorth'] * .95
+        selloutPrice = self.account['highestWorth'] * .985
 
         # calculate the current worth in dollars of bitcoin quantity
         worth = price * quantityBought
 
+        # writes new account high
         if worth > highestWorth:
-            account['highestWorth'] = worth
-            with open('account.json', 'w') as f:
-                account = json.dump(account, f)
 
+            self.account['highestWorth'] = worth
+            self.account['selloutPrice'] = ((highestWorth - purchasePrice) * 0.92 + purchasePrice)
+
+
+            with open('account.json', 'w') as f:
+                account = json.dump(self.account, f)
+
+        # sends alert if the price drops below the thresh-hold that was set
         if worth < selloutPrice:
             self.sendPriceAlert(self.number, '~\\\\~ CHECK BITCOIN WALLET ~//~')
             exit()
@@ -54,9 +58,15 @@ class Drew():
 
         # print the gain / loss of the purhcase
         if difference > 0:
-            print('{}\tMONEY GAINED:\t$ {}'.format(datetime.datetime.now(), difference))
+            str = '{}\tMONEY GAINED:\t$ {}\tWORTH: $ {}'.format(datetime.datetime.now(), difference, worth)
+            print(str)
         if difference < 0:
-            print('MONEY LOST:\t$ {}'.format(datetime.datetime.now(), difference))
+            str = 'MONEY LOST:\t$ {}'.format(datetime.datetime.now(), difference)
+            print(str)
+
+        if str != None:
+            with open('report.txt', 'a') as f:
+                f.write(str + '\n')
 
     def sendPriceAlert(self, number, message):
         client = Client(self.sid, self.token)
