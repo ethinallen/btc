577925c8ccb9c82194ee1b223724c6393fd96c94
Drew Emery <abemery@clemson.edu>
2019-06-21 23:52:26 -0400

    fixed sellout thresh-hold; added alerts.json so I can add more custom alerts

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class Drew():
         self.token = self.account['authToken']
         self.number = self.account['phoneNumber']
 
+        with open('alerts.json', 'r') as f:
+            self.alerts = json.load(f)
+
+
     def fetch(self):
         # get the
         r = requests.get('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
@@ -35,10 +39,11 @@ class Drew():
         purchasePrice = self.account['purchasePrice']
         highestWorth = self.account['highestWorth']
 
-        selloutPrice = self.account['highestWorth'] * .985
+        selloutPrice = self.account['selloutPrice']
 
         # calculate the current worth in dollars of bitcoin quantity
         worth = price * quantityBought
+        self.account['gain'] = worth - purchasePrice
 
         # writes new account high
         if worth > highestWorth:
@@ -46,28 +51,31 @@ class Drew():
             self.account['highestWorth'] = worth
             self.account['selloutPrice'] = ((highestWorth - purchasePrice) * 0.92 + purchasePrice)
 
-
             with open('account.json', 'w') as f:
                 account = json.dump(self.account, f)
 
         # sends alert if the price drops below the thresh-hold that was set
         if worth < selloutPrice:
-            self.sendPriceAlert(self.number, '~\\\\~ CHECK BITCOIN WALLET ~//~')
+            string = self.alerts['priceDrop']
+            self.sendPriceAlert(self.number, self.formatMessage(string))
             exit()
+
+        # the difference between the present worth and the purchase price
         difference = worth - purchasePrice
 
         # print the gain / loss of the purhcase
         if difference > 0:
             str = '{}\tMONEY GAINED:\t$ {}\tWORTH: $ {}'.format(datetime.datetime.now(), difference, worth)
-            print(str)
+
         if difference < 0:
             str = 'MONEY LOST:\t$ {}'.format(datetime.datetime.now(), difference)
-            print(str)
 
+        # if a string is crafted then write it to the report
         if str != None:
             with open('report.txt', 'a') as f:
                 f.write(str + '\n')
 
+    # send alert about the price
     def sendPriceAlert(self, number, message):
         client = Client(self.sid, self.token)
         message = client.messages.create(
@@ -75,9 +83,17 @@ class Drew():
             from_="+19389999671",
             body=message)
 
+    def formatMessage(self, string):
+        string = '~\\\\~ ' + string + ' ~//~'
+        return string
+
 if __name__ == '__main__':
 
     drew = Drew()
+
     while True:
-        drew.compare(drew.fetch())
-        time.sleep(15)
+        try:
+            drew.compare(drew.fetch())
+            time.sleep(15)
+        except:
+            pass
